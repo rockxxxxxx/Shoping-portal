e7a8649390f5cc2a4647c4f8b2ef278f6dfb5832
rockxxxxxx <78143717+rockxxxxxx@users.noreply.github.com>
2022-10-21 00:39:12 +0530

    Added lazy loading and suspence

--- src/App.js
+++ src/App.js
@@ -1,40 +1,57 @@
+import React, { Suspense } from "react";
 import "./App.css";
 import Navbar from "./components/routers/navigation/Navbar";
 import { Route, Routes, Navigate } from "react-router-dom";
-import Home from "./components/home/Home";
-import About from "./components/about/About";
-import Store from "./components/store/Store";
-import Contact from "./components/contact/Contact";
-import ProductDetails from "./components/product-details/ProductDetails";
-import Login from "./components/login/Login";
-import SignUp from "./components/signup/Signup";
-import ChangePassword from "./components/login/ChangePassword";
+
 import { useContext } from "react";
 import { LoginContext } from "./components/context/login-context";
-import Error from "./components/error/Error";
-import Checkout from "./components/checkout/Checkout";
+
+const Home = React.lazy(() => import("./components/home/Home"));
+const About = React.lazy(() => import("./components/about/About"));
+const Store = React.lazy(() => import("./components/store/Store"));
+const Contact = React.lazy(() => import("./components/contact/Contact"));
+const ProductDetails = React.lazy(() =>
+  import("./components/product-details/ProductDetails")
+);
+const Login = React.lazy(() => import("./components/login/Login"));
+const SignUp = React.lazy(() => import("./components/signup/Signup"));
+const ChangePassword = React.lazy(() =>
+  import("./components/login/ChangePassword")
+);
+const Error = React.lazy(() => import("./components/error/Error"));
+const Checkout = React.lazy(() => import("./components/checkout/Checkout"));
 
 function App() {
   const { isLoggedIn } = useContext(LoginContext);
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Navbar />}>
-          <Route path="/home" element={<Home />} />
-          <Route path="/" element={<Navigate replace to="/home" />} />
-          {isLoggedIn && <Route path="/store" element={<Store />} />}
-          <Route path="/about" element={<About />} />
-          <Route path="/contact-us" element={<Contact />} />
-          <Route path="/productDetails" element={<ProductDetails />} />
-          {!isLoggedIn && <Route path="/login" element={<Login />} />}
-          {!isLoggedIn && <Route path="/signup" element={<SignUp />} />}
-          {isLoggedIn && (
-            <Route path="/changepassword" element={<ChangePassword />} />
-          )}
-          {isLoggedIn && <Route path="/checkout" element={<Checkout />} />}
-          <Route path="*" element={<Error />} />
-        </Route>
-      </Routes>
+      <Suspense
+        fallback={
+          <div className="ui segment sementic-loader">
+            <div className="ui active inverted dimmer">
+              <div className="ui large text loader">Loading</div>
+            </div>
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/" element={<Navigate replace to="/home" />} />
+            {isLoggedIn && <Route path="/store" element={<Store />} />}
+            <Route path="/about" element={<About />} />
+            <Route path="/contact-us" element={<Contact />} />
+            <Route path="/productDetails" element={<ProductDetails />} />
+            {!isLoggedIn && <Route path="/login" element={<Login />} />}
+            {!isLoggedIn && <Route path="/signup" element={<SignUp />} />}
+            {isLoggedIn && (
+              <Route path="/changepassword" element={<ChangePassword />} />
+            )}
+            {isLoggedIn && <Route path="/checkout" element={<Checkout />} />}
+            <Route path="*" element={<Error />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 }
